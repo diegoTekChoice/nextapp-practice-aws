cb90bc92fdc87137810d574eb80a844bd207f5e4
Diego Herrera <diego.herrera@tekchoice.com>
2023-06-09 11:11:19 -0600

    Fix ecs

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-        <h2>host:{window.location.host}</h2>
-        <h2>hostname:{window.location.hostname}</h2>
         <h6>second version - ECS</h6>
     </>
   )
